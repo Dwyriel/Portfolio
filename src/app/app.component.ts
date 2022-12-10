5aff58860ae45e324a036f482087d663594a5a4b
Dwyriel <dwyriel333@hotmail.com>
2022-12-10 07:14:55 -0300

    Reformatted project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserDbService} from "./services/user-db.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Portfolio';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'general-dwy',
-    appId: '1:538529265648:web:3a0211911f759612758f35',
-    storageBucket: 'general-dwy.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyBfxhdXEy5JxHbzPZa70S_xPoFXajt1pN0',
-    authDomain: 'general-dwy.firebaseapp.com',
-    messagingSenderId: '538529265648',
-  },
-  production: false
+    firebase: {
+        projectId: 'general-dwy',
+        appId: '1:538529265648:web:3a0211911f759612758f35',
+        storageBucket: 'general-dwy.appspot.com',
+        locationId: 'us-central',
+        apiKey: 'AIzaSyBfxhdXEy5JxHbzPZa70S_xPoFXajt1pN0',
+        authDomain: 'general-dwy.firebaseapp.com',
+        messagingSenderId: '538529265648',
+    },
+    production: false
 };
 
 /*
